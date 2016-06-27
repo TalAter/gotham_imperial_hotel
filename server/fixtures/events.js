be91240afecf41342f2f8726b6eeb83833b09c81
Tal Ater <tal@talater.com>
2016-06-27 16:36:39 +0300

    Events fixtures will now include random dates for some events or events within a certain date in the current year for others

--- server/fixtures/events.js
+++ server/fixtures/events.js
@@ -1,27 +1,41 @@
+var moment = require('moment');
+
 const eventFixtures = [
   {
     "title": "New Year's Eve Party",
     "description": "The Gotham Imperial invites you to celebrate our annual New Year's Eve party.",
     "img": "/img/event-new-year.jpg",
-    "date": "2019-12-31"
+    "date": "12/31"
   },
   {
     "title": "Rooftop Party",
     "description": "Come celebrate the re-opening of our famous rooftop bar.",
-    "img": "/img/event-new-year.jpg",
-    "date": "2019-11-15"
+    "img": "/img/event-new-year.jpg"
   },
   {
     "title": "Halloween Party",
     "description": "Come celebrate the scariest night of the year with us.",
     "img": "/img/event-new-year.jpg",
-    "date": "2019-10-31"
+    "date": "10/31"
   }
 ];
 
 var events = [];
 
+// Create the events array
 for (let event of eventFixtures) {
+  // Make up some dates for the events
+  let date;
+  if (event.date) {
+    // If certain events have specific date requirements (e.g. new year's eve, Halloween)
+    // use that date and add the year
+    date = moment(moment().format('YYYY/')+'/'+event.date, "YYYY/MM/DD");
+  } else {
+    // Else randomize a date within the next 30 days
+    date = moment().add(Math.floor(Math.random() * 30), 'days');
+  }
+
+  event.date = date.format('l');
   events.push(event);
 }
 
