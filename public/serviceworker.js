da4581315e0c3a8d4e0a51add12974809c41024e
Tal Ater <tal@talater.com>
2016-07-02 17:03:56 +0300

    Updated service worker to cache index.html and its assets

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,11 +1,26 @@
-var CACHE_NAME = 'gih-cache-v4';
+var CACHE_NAME = 'gih-cache-v5';
 var CACHED_URLS = [
-  '/sw-index.html',
+  // Our HTML
+  '/index.html',
+  // Stylesheets
+  '/css/gih.css',
   'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
-  '/img/logo.png'
+  'https://fonts.googleapis.com/css?family=Lato:300,600,900',
+  // JavaScript
+  'https://code.jquery.com/jquery-3.0.0.min.js',
+  '/js/app.js',
+  // Images
+  '/img/logo.png',
+  '/img/logo-header.png',
+  '/img/event-calendar-link.jpg',
+  '/img/logo-top-background.png',
+  '/img/jumbo-background.jpg',
+  '/img/about-hotel-spa.jpg',
+  '/img/about-hotel-luxury.jpg'
 ];
 
 self.addEventListener('install', function(event) {
+  // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(CACHED_URLS);
@@ -20,7 +35,7 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('/sw-index.html');
+          return caches.match('/index.html');
         }
       });
     })
