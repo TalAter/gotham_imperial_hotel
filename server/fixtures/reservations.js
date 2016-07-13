5a46747cc8e1c6924d680e591888f43cd0a325d3
Tal Ater <tal@talater.com>
2016-07-13 15:29:07 +0300

    Added reservation fixtures

--- server/fixtures/reservations.js
+++ server/fixtures/reservations.js
@@ -1,1 +1,22 @@
-module.exports = [];
+var moment = require('moment');
+var _ = require('lodash');
+
+var reservations = [];
+var date = moment();
+
+for (let i = 0; i < 2; i++) {
+  date.add(_.random(5,30), 'days');
+  let reservation = {
+    "check_in":   date.format('MMMM Do YYYY'),
+    "nights":     _.random(1,4),
+    "guests":     _.random(1,3),
+    "status":     "confirmed",
+    "id":         _.random(8020000,8060000),
+    "booked_on":  moment().subtract(_.random(1,6), 'days').format('MMMM Do YYYY')
+  };
+  reservation.price = reservation.nights*_.random(200,249);
+
+  reservations.push(reservation);
+}
+
+module.exports = reservations;
