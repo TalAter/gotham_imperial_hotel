93a5c2364058a14407ff5adb4c8e8ed11f22fd25
Tal Ater <tal@talater.com>
2016-07-14 15:51:09 +0300

    Added functionality for homepage booking widget

--- server/index.js
+++ server/index.js
@@ -21,12 +21,16 @@ app.get('/events.json', function(req, res) {
 });
 
 app.get('/make-reservation', function(req, res) {
-  let id          = req.query['id'];
-  let arrivalDate = req.query['arrivalDate'];
-  let nights      = req.query['nights'];
-  let guests      = req.query['guests'];
+  let id          = req.query['id'] || Date.now().toString().substring(3,11);
+  let arrivalDate = req.query['arrivalDate'] || req.query['form--arrival-date'];
+  let nights      = req.query['nights'] || req.query['form--nights'];
+  let guests      = req.query['guests'] || req.query['form--guests'];
   let reservationStatus = reservations.make(id, arrivalDate, nights, guests);
-  res.json(reservationStatus);
+  if (req.xhr) {
+    res.json(reservationStatus);
+  } else {
+    res.redirect('/my-account');
+  }
 });
 
 
