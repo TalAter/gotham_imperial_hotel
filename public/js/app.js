670cc291023e00392a1225c28b9b3e5210113f27
Tal Ater <tal@talater.com>
2016-07-20 16:36:34 +0300

    Extracted rendering logic to functions "below the fold"

--- public/js/app.js
+++ public/js/app.js
@@ -9,16 +9,29 @@ if ('serviceWorker' in navigator) {
 
 $(document).ready(function() {
   // Fetch and render upcoming events in the hotel
-  $.getJSON('/events.json', function(data) {
-    data.forEach(function(event) {
-      $(
-        '<div class="col-lg-2 col-md-4 col-sm-6 event-container"><div class="event-card">'+
-        '<div class="event-date">'+event.date+'</div>'+
-        '<img src="'+event.img+'" alt="'+event.title+'" class="img-responsive" />'+
-        '<h4>'+event.title+'</h4>'+
-        '<p>'+event.description+'</p>'+
-        '</div></div>'
-      ).insertBefore('#events-container div.calendar-link-container');
-    });
-  });
+  $.getJSON('/events.json', renderEvents);
 });
+
+
+
+
+/* ************************************************************ */
+/* The code below this point is used to render to the DOM. It   */
+/* completely ignores common sense principles like separation   */
+/* of concerns as a trade off for readability.                  */
+/* You can ignore it, or you can send angry tweets about it to  */
+/* @TalAter                                                     */
+/* ************************************************************ */
+
+var renderEvents = function(data) {
+  data.forEach(function(event) {
+    $(
+      '<div class="col-lg-2 col-md-4 col-sm-6 event-container"><div class="event-card">'+
+      '<div class="event-date">'+event.date+'</div>'+
+      '<img src="'+event.img+'" alt="'+event.title+'" class="img-responsive" />'+
+      '<h4>'+event.title+'</h4>'+
+      '<p>'+event.description+'</p>'+
+      '</div></div>'
+    ).insertBefore('#events-container div.calendar-link-container');
+  });
+};
