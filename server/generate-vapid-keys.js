d93916666e03e955bae81cdbb26d6084f9ee7b81
Tal Ater <tal@talater.com>
2016-10-19 16:47:05 +0300

    Updated generate-vapid-keys.js to create a vapid-details.js file (which is ignored by git)

--- server/generate-vapid-keys.js
+++ server/generate-vapid-keys.js
@@ -1,9 +1,26 @@
 var webpush = require('web-push');
-
+var fs = require('fs');
 var vapidKeys = webpush.generateVAPIDKeys();
 
-console.log('Public Key: ');
+console.log('Your new public key is:');
 console.log(vapidKeys.publicKey);
+console.log('This key should be used as the applicationServerKey when creating a subscription');
 console.log();
-console.log('Private Key:');
-console.log(vapidKeys.privateKey);
+
+var vapidDetailsContents = `module.exports = {
+  GCMAPIKey: 'YOUR_GCM_API_KEY',
+  subject: 'mailto:your@email.com',
+  publicKey: '${vapidKeys.publicKey}',
+  privateKey: '${vapidKeys.privateKey}'
+};`;
+
+var vapidDetailsFilePath = __dirname+'/vapid-details.js';
+
+fs.writeFile(vapidDetailsFilePath, vapidDetailsContents, function(err) {
+  if (err) {
+    console.log('Error creating vapid-details.js');
+    console.log(err);
+  }
+  console.log(vapidDetailsFilePath+' created.');
+  console.log('You should update it with your GCM API key, and email');
+});
