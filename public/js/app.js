9a3c62bea8022ff5b01be7dfec98f2ffcff95c56
Tal Ater <tal@talater.com>
2017-04-19 13:37:08 +0300

    Updated service worker registration messages

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/serviceworker.js").then(function(registration) {
-    console.log("Service Worker registered successfully with scope: ", registration.scope);
+    console.log("Service Worker registered with scope:", registration.scope);
   }).catch(function(err) {
-    console.log("Service Worker registration failed: ", err);
+    console.log("Service Worker registration failed:", err);
   });
 }
 
