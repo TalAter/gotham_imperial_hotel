11006d8ad39771a736f0385c184c1d3eb2003da5
guaca <estela.francoc@gmail.com>
2018-06-12 21:05:24 +0200

    edit serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,18 +1,28 @@
-var responseContent = "<html>" +
-  "<body>" +
-  "<style>" +
-  "body {text-align: center; background-color: #333; color: #eee;}" +
-  "</style>" +
-  "<h1>Gotham Imperial Hotel</h1>" +
-  "<p>There seems to be a problem with your connection.</p>" +
-  "<p>Come visit us at 1 Imperial Plaza, Gotham City for free WiFi.</p>" +
-  "</body>" +
-  "</html>";
-
+var CACHE_NAME = "gih-cache";
+var CACHED_URLS = [
+  "/index-offline.html",
+  "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
+  "/css/gih-offline.css",
+  "/img/jumbo-background-sm.jpg",
+  "/img/logo-header.png"
+];
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
 self.addEventListener("fetch", function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
-      return new Response(responseContent, {headers: {"Content-Type": "text/html"}});
+      return caches.match(event.request, {ignoreSearch: true}).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("index-offline.html");
+        }
+      });
     })
   );
 });
